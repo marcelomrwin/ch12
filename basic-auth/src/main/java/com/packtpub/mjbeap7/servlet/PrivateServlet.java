ccd1867968568623e9b043033ac0624fc83b4bd6
foogaro <l.fugaro@gmail.com>
2016-05-13 11:36:31 +0200

    REmoved useless comments.

--- basic-auth/src/main/java/com/packtpub/mjbeap7/servlet/PrivateServlet.java
+++ basic-auth/src/main/java/com/packtpub/mjbeap7/servlet/PrivateServlet.java
@@ -1,19 +1,3 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.packtpub.mjbeap7.servlet;
 
 import java.io.IOException;
@@ -28,13 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * A simple secured Servlet. Upon successful authentication and authorization the Servlet will print details of the user and
- * authentication. Servlet security is implemented using annotations.
- *
- * @author Sherif Makary
- *
- */
 @SuppressWarnings("serial")
 @WebServlet("/PrivateServlet")
 @ServletSecurity(@HttpConstraint(rolesAllowed = { "mjeap7" }))
